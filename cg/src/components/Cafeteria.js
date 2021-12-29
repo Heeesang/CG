3c9c9c0ddca302f4212d9374d0c915254ad321b8
Heesang <80827042+Heeesang@users.noreply.github.com>
2021-12-29 14:26:16 +0900

    BUG

--- cg/src/components/Cafeteria.js
+++ cg/src/components/Cafeteria.js
@@ -1,23 +1,42 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
+import axios from "axios";
 import "./Cafeteria.css";
-
-const today = () => {
-  let now = new Date();
-  let todayMonth = now.getMonth() + 1;
-  let todayDate = now.getDate();
-  if (todayMonth == 13) {
-    todayMonth = 1;
-  }
-  return todayMonth + "월" + todayDate + "일";
-};
+import Parser from "html-react-parser";
 
 const Cafeteria = () => {
-  const [day, setDay] = useState(today());
+  const [datas, setData] = useState([]);
+  const [value, setValue] = useState("비아고등학교");
 
+  const fetchData = async () => {
+    try {
+      const response1 = await axios.get(
+        `https://open.neis.go.kr/hub/schoolInfo?Key=813eb00ac6ae4e4b9bf6cc5254404138&Type=json&pIndex=1&pSize=10&SCHUL_NM=${value}`
+      );
+      console.log(response1.data);
+      const response = await axios.get(
+        "https://open.neis.go.kr/hub/mealServiceDietInfo?Key=813eb00ac6ae4e4b9bf6cc5254404138&Type=json&pIndex=1&pSize=1000&ATPT_OFCDC_SC_CODE=F10&SD_SCHUL_CODE=7380292"
+      );
+      console.log(response);
+      setData(response.data.mealServiceDietInfo[1].row);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const onChange = () => {
+    setValue(value);
+  };
+
+  const onSubmit = (e) => {
+    fetchData();
+  };
   return (
     <div className="Cafeteria">
       <div className="Cafeteria_cont">
-        <h1>{day}</h1>
+        <form onSubmit={onSubmit}>
+          <input value={value} onChange={onChange} />
+        </form>
+        <h2>{Parser(datas[0].DDISH_NM)}</h2>
       </div>
     </div>
   );
